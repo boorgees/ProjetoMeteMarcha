e67c9b1485031bfae67b371f48cef325c83963f6
CHRISTIAN DE ABREU BORGES <christian.borges2@alunos.sc.senac.br>
2025-01-22 19:11:30 -0300

    adicao de clientes

---
+++ MeteMarcha/Models/Clientes/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MeteMarcha.Models.Clientes
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Situacao { get; set; }
+    }
+}
